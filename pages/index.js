8aa51fd40bd4a6631130324a6e4bb42e48fefbe1
SamuelHSA <111927634+SamuelHSA@users.noreply.github.com>
2022-10-15 14:13:04 -0300

    Componentes

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,25 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { useState, useEffect } from "react";
+import Home from "../componentes/home";
+import Login from "../componentes/login";
+import UsuarioService from "../services/UsuarioService";
 
-export default function Home() {
-  return (
-    <h1>Olá mundo!</h1>
-  )
+const usuarioService = new UsuarioService();
+export default function Index() {
+  const [estaAutenticado, setEstaAutenticado] = useState(null);
+
+  useEffect(() => {
+    setEstaAutenticado(
+      usuarioService.estaAutenticado()
+    );
+  }, []);
+
+  if (estaAutenticado === null) {
+    return null;
+  }
+
+  if (estaAutenticado) {
+    return <Home />;
+  }
+
+  return <Login aposAutenticacao={() => setEstaAutenticado(true)} />;
 }
